6e05e2c00efd32f677a09f4a116aa9402fb5b747
Chalaka <chalaka.jayaranga@pagero.com>
2023-02-08 21:51:10 +0530

    React Query - usePost

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,30 +1,10 @@
 import React from 'react'
 import axios from 'axios'
+import {useQuery} from "react-query";
 
 export const fetchPost = (postId) =>
   axios.get(`/api/posts/${postId}`).then((res) => res.data)
 
 export default function usePost(postId) {
-  const [state, setState] = React.useReducer((_, action) => action, {
-    isLoading: true,
-  })
-
-  const fetch = React.useCallback(async () => {
-    setState({ isLoading: true })
-    try {
-      const data = await fetchPost(postId)
-      setState({ isSuccess: true, data })
-    } catch (error) {
-      setState({ isError: true, error })
-    }
-  }, [postId])
-
-  React.useEffect(() => {
-    fetch()
-  }, [fetch])
-
-  return {
-    ...state,
-    fetch,
-  }
+  return  useQuery(['posts', postId], () => fetchPost(postId))
 }
