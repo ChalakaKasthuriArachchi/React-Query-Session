79c335098c1374aba86b8df36f371866b4019755
Chalaka <chalaka.jayaranga@pagero.com>
2023-02-09 19:37:24 +0530

    usePost - Speed up initial render

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import axios from 'axios'
-import {useQuery} from "react-query";
+import {queryCache, useQuery} from "react-query";
 
 export const fetchPost = (postId) =>
   axios.get(`/api/posts/${postId}`).then((res) => res.data)
 
 export default function usePost(postId) {
-  return  useQuery(['posts', postId], () => fetchPost(postId))
+  return  useQuery(['posts', postId], () => fetchPost(postId), {
+    initialData: () => {
+      return queryCache.getQueryData('posts')?.find(post => post.id === postId)
+    },
+    initialStale: true
+  })
 }
