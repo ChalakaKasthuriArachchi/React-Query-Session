2f601f401e6bcebce806c819fae66abf79eb327c
Chalaka <chalaka.jayaranga@pagero.com>
2023-02-14 19:19:40 +0530

    Final Change

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-import {useQuery} from "react-query";
+import {queryCache, useQuery} from "react-query";
+import {fetchPost} from "./usePost";
 
 export default function usePosts() {
   return  useQuery(
@@ -8,6 +9,18 @@ export default function usePosts() {
       () => axios.get('/api/posts').then((res) => res.data), {
         cacheTime: 10 * 60 * 1000,
         staleTime: 0,
-        refetchOnWindowFocus: true
+        refetchOnWindowFocus: true,
+        onMutate: () => {
+            return queryCache.getQueryData('posts');
+        },
+        onSuccess: (savedPosts, _ , existingPosts) => {
+            if(savedPosts?.length !== existingPosts?.length) {
+                return savedPosts?.map(post => {
+                    if(!queryCache.getQueryData(['posts', post.id])) // if the post is not in the cache
+                        queryCache.prefetchQuery(['posts', post.id], () => fetchPost(post.id))
+                })
+            }
+            return null;
+        }
       })
 }
